89addab1f6c951d63c805dae80c425eac7ab518f
drogi17 <38162697+drogi17@users.noreply.github.com>
2020-02-04 10:34:32 +0200

    Update load_config.py

--- load_config.py
+++ load_config.py
@@ -7,37 +7,6 @@ CURSOR_TYPES   = {
 }
 
 
-# def load_config(file, cfg_dict):
-#     if os.path.exists(file):
-#         with open(file, 'r') as f:
-#             for line in f.readlines():
-#                 coments = line.find('#')
-#                 if coments != -1:
-#                     line = line[:coments]
-#                 line = str(line).replace('\n', '')
-#                 if line[:4] == 'SET ':
-#                     setting = str(line).replace('SET ', '').replace(' ', '').replace('  ', '').split('=')
-#                     if len(setting) > 1:
-#                         if setting[1] == 'False':
-#                             setting[1] = False
-#                         elif setting[1] == 'True':
-#                             setting[1] = True
-#                         elif str(setting[1]).isdigit():
-#                             setting[1] = int(setting[1])
-#                         elif ',' in setting[1]:
-#                             array = str(setting[1]).split(',')
-#                             nomb = 0
-#                             while nomb <= len(array)-1:
-#                                 if str(array[nomb]).isdigit():
-#                                     array[nomb] = int(array[nomb])
-#                                 nomb += 1
-#                             setting[1] = array
-#                         if setting[0] in cfg_dict:
-#                             cfg_dict[setting[0]] = setting[1]
-#         return cfg_dict
-#     else:
-#         print('default.cfg not found')
-#         sys.exit()
 
 class Config:
     cfg_dict = {
